f1568ff857b1bd18a21c112b220df5f807764b90
lokesh2512 <lokeshgupta2512@gmail.com>
2023-09-25 10:50:21 +0530

    updated frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NewHomeComponent } from './new-home/new-home.component';
 import { FooterComponent } from './footer/footer.component';
+import{FormsModule,ReactiveFormsModule} from '@angular/forms'
 //import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CardsectionComponent } from './cardsection/cardsection.component';
 import { TileSectionComponent } from './tile-section/tile-section.component';
+import { NewsSectionComponent } from './news-section/news-section.component';
 
 
 
@@ -20,6 +22,8 @@ import { TileSectionComponent } from './tile-section/tile-section.component';
     FooterComponent,
     CardsectionComponent,
     TileSectionComponent,
+    NewsSectionComponent,
+    
 
     
 
@@ -27,6 +31,9 @@ import { TileSectionComponent } from './tile-section/tile-section.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+    
     
   ],
   providers: [],
