7df9c1141b13769c1b1636180969fc4b6b235240
PandeyHariOm07 <05altamash05@gmail.com>
2023-09-26 19:43:52 +0530

    implemented the validations

--- src/app/allComponents/add-doctor/add-doctor.component.ts
+++ src/app/allComponents/add-doctor/add-doctor.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CurewellService } from 'src/app/services/curewell.service';
 
@@ -9,22 +10,35 @@ import { CurewellService } from 'src/app/services/curewell.service';
 })
 export class AddDoctorComponent {
   name: string = "";
+  submitted: boolean = false;
+  add!: FormGroup;
+  regex:string = "^[a-zA-Z ]*$";
+
   constructor(private route: ActivatedRoute, private _cureWellService: CurewellService, private router: Router) { }
   ngOnInit(): void {
     this.name = this.route.snapshot.params['DoctorName']
+      this.add = new FormGroup({
+      name: new FormControl(this.name, [Validators.required, Validators.minLength(3),Validators.pattern(this.regex)])
+    });
   }
 
   //To Add Doctor Details
   addDoctor(doctorName: string) {
-    this._cureWellService.AddDoctor(this.name).subscribe(
-      value => {
-        if (value) {
-          //alert("Doctor Added Successfully :)")
+    this.submitted = true;
+    if (this.add.get('name')?.valid) {
+      this._cureWellService.AddDoctor(this.name).subscribe(
+        value => {
+          if (value) {
+            //alert("Doctor Added Successfully :)")
+            this.router.navigate(['/ViewDoctor'])
+          }
+          else { alert('Doctor Added Successfully :)') }
           this.router.navigate(['/ViewDoctor'])
         }
-        else { alert('Doctor Added Successfully :)') }
-        this.router.navigate(['/ViewDoctor'])
-      }
-    )
-   }
+      )
+    }
+  }
+  get f(): { [key: string]: AbstractControl }{
+    return this.add?.controls;
+  }
 }
--- src/app/allComponents/update-doctor/update-doctor.component.ts
+++ src/app/allComponents/update-doctor/update-doctor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Doctor } from 'src/app/curewell-interfaces/doctor';
 import { CurewellService } from 'src/app/services/curewell.service';
@@ -10,26 +11,39 @@ import { CurewellService } from 'src/app/services/curewell.service';
 })
 export class UpdateDoctorComponent implements OnInit {
   doctorList: Doctor[] = [];
+  submitted: boolean = false;
+  update!: FormGroup;
   id: number = 0;
   name: string = "";
+  regex:string = "^[a-zA-Z ]*$";
+
   constructor(private route: ActivatedRoute, private _cureWellService: CurewellService, private router: Router) { }
   ngOnInit(): void {
     this.id = this.route.snapshot.params['DoctorId'];
     this.name = this.route.snapshot.params['DoctorName']
+    this.update = new FormGroup({
+      name: new FormControl(this.name, [Validators.required, Validators.minLength(3),Validators.pattern(this.regex)])
+    });
   }
   //To Update The New Updated Doctor's Name in Database
   updateDoctor(doctorname: string) {
-    this._cureWellService.updateDoctor(this.id, this.name).subscribe(
-      value => {
-        if (value) {
-          alert("Updated Successfully :)")
-          this.router.navigate(['/ViewDoctor'])
+    this.submitted = true;
+    if (this.update.get('name')?.valid) {
+      this._cureWellService.updateDoctor(this.id, this.name).subscribe(
+        value => {
+          if (value) {
+            alert("Updated Successfully :)")
+            this.router.navigate(['/ViewDoctor'])
+          }
+          else {
+            alert("Sorry, Could Not Update :(")
+            this.router.navigate(['/ViewDoctor'])
+          }
         }
-        else {
-          alert("Sorry, Could Not Update :(")
-          this.router.navigate(['/ViewDoctor'])
-        }
-      }
-    )
-  }
+      )
     }
+  }
+  get f(): { [key: string]: AbstractControl }{
+    return this.update?.controls;
+  }
+}
