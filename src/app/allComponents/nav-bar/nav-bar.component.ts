e63f4c3b98e87763d3cdebea0d9fa5a0dead6250
lokesh2512 <lokeshgupta2512@gmail.com>
2023-09-26 15:50:17 +0530

    updated login

--- src/app/allComponents/nav-bar/nav-bar.component.ts
+++ src/app/allComponents/nav-bar/nav-bar.component.ts
@@ -9,51 +9,52 @@ import { MatchValidation } from './match-validation';
 })
 export class NavBarComponent implements OnInit {
   LoginForm !: FormGroup;
-  SignUpform !: FormGroup;
+ // SignUpform !: FormGroup;
   submitted: boolean = false;
-  submittedSign: boolean = false;
+  //submittedSign: boolean = false;
+  passwordRegex: string = "^[(a-zA-Z_)+(0-9){2,2}]$";
 
   constructor(private fb: FormBuilder) { };
   ngOnInit(): void {
     this.LoginForm = this.fb.group({
       userName: [null, [Validators.required]],
-      passWord: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(10)]]
+      passWord: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(10),Validators.pattern(this.passwordRegex)]]
 
     });
-    this.SignUpform = this.fb.group({
-      UserName: [null, [Validators.required]],
-      emailId: [null, [Validators.required, Validators.email]],
-      passWord: [null, [Validators.required, Validators.minLength(8)]],
-      confirmPass: [null, [Validators.required, Validators.minLength(8)]]
+    // this.SignUpform = this.fb.group({
+    //   UserName: [null, [Validators.required]],
+    //   emailId: [null, [Validators.required, Validators.email]],
+    //   passWord: [null, [Validators.required, Validators.minLength(8)]],
+    //   confirmPass: [null, [Validators.required, Validators.minLength(8)]]
 
-    }, { validators: MatchValidation("passWord", "confirmPass") }
-    )
+    // }, { validators: MatchValidation("passWord", "confirmPass") }
+   // )
   }
   OnSubmit() {
     this.submitted = true;
     console.log(this.LoginForm);
 
   }
-  OnSubmitSignUp() {
-    this.submittedSign = true;
-    console.log(this.SignUpform);
+  // OnSubmitSignUp() {
+  //   this.submittedSign = true;
+  //   console.log(this.SignUpform);
 
-  }
+  // }
   getControl(controlName: string): AbstractControl {
     return this.LoginForm.controls[controlName];
 
   }
-  getControlSign(controlName: string): AbstractControl {
-    return this.SignUpform.controls[controlName];
+  // getControlSign(controlName: string): AbstractControl {
+  //   return this.SignUpform.controls[controlName];
 
-  }
+ // }
   get f(): { [controlname: string]: AbstractControl } {
     return this.LoginForm.controls;
   }
 
-  get g(): { [controlname: string]: AbstractControl } {
-    return this.SignUpform.controls;
-  }
+  // get g(): { [controlname: string]: AbstractControl } {
+  //   return this.SignUpform.controls;
+  // }
 
 
 
